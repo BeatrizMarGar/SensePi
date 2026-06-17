46eff4a25cc938c02f223d7f969eea20abdc9bdc
BeatrizMarGar <martin_beatriz94@hotmail.com>
2026-06-17 23:40:09 +0200

    feat: add SensorPanel component with polling every 2s

---
+++ frontend/src/components/SensorPanel.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+
+function SensorPanel(){
+
+	const [data, setData] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+
+	useEffect(() => {
+	
+		const fetchSensor = async () => {
+			try {
+				const res = await fetch('http://10.1.1.151:3001/sensor')
+				if (!res.ok) throw new Error ('Error del servidor')
+				const json = await res.json()
+				setData(json)
+				setError(null)
+			} catch (err) {
+				setError(err.message)
+			} finally{
+				setLoading(false)
+			}
+		}
+	
+	fetchSensor()
+
+	const intervalo = setInterval(fetchSensor, 2000)
+	return () => clearInterval(intervalo)
+	}, [])
+
+	if (loading) return <p>Cargando información del sensor...</p>
+	if (error) return <p>Error: {error}</p>
+
+	return (
+		<div>
+			<h2>Sensor DHT11</h2>
+			<p>Temperatura: {data.temperatura} ºC</p>
+			<p>Humedad: {data.humedad} %</p>
+			<p>Estado: {data.estado}</p>
+		</div>
+
+	)}	
+
+export default SensorPanel
